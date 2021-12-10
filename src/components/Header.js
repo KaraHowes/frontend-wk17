43988c6e0b4e359e752a5f55707db97f85182821
Kara Howes <karahowes@Karas-MacBook-Air.local>
2021-12-10 22:15:56 +0100

    added alt tag

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Header = () => {
 
     return(
-        <img className="header-img" src="/assets/nobel.jpg"/>
+        <img className="header-img" src="/assets/nobel.jpg" alt="Nobel-prize medal"/>
     )
 }
 export default Header
